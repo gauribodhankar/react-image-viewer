c1b16e9cdd618d3d12cd9665628295af2cd2df30
Gauri Bodhankar <gbodhankar@stubhub.com>
2018-01-30 19:44:47 -0800

    Added fetch call and parsed JSON to create combined image objects with urls
    Combining ConvertedImage & DeploymentRecord objects to get a single image object

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,16 +1,40 @@
-import React from 'react'
-class ImageViewer extends React.Component {
+import React, {Component} from 'react'
+
+class ImageViewer extends Component {
   
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
+  }
   componentWillMount() {
     this.fetchImages();
   }
   
   fetchImages() {
-    fetch('/data/imageData.json', {
-      type: 'GET',
-      contentType: 'application/json'
-    }).then(function(response){
-      console.log('got a response');
+    fetch(
+      '/data/imageData.json'
+    ).then(response => {
+      // TODO: add error handling
+      return response.json();
+    }).then((imageData) => {
+      let imageArray = [];
+      imageData.reduce((prevImage, image) => {
+        prevImage = prevImage || {};
+
+        if(prevImage.assetId === image.assetId) {
+          Object.assign(prevImage, image);
+          prevImage.url = `https://secure.netflix.com/us/boxshots/${prevImage.dir}/${prevImage.filename}`;
+          imageArray.push(prevImage);
+        } else {
+          prevImage = image;
+        }
+        return prevImage;
+      });
+      // TODO: remove console statements
+      console.log(imageArray);
+      this.setState({images: imageArray});
     });
   }
 
