2396bcfd5638f366c7fb3456e4d00d066309963c
Gauri Bodhankar <gbodhankar@stubhub.com>
2018-01-30 17:43:28 -0800

    Added fetch call

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,9 +1,25 @@
 import React from 'react'
-const ImageViewer = () => {
-  return (
-    <div className='hello-world'>
-      <h1>Images will be displayed here</h1>
-    </div>
-  )
+class ImageViewer extends React.Component {
+  
+  componentWillMount() {
+    this.fetchImages();
+  }
+  
+  fetchImages() {
+    fetch('/data/imageData.json', {
+      type: 'GET',
+      contentType: 'application/json'
+    }).then(function(response){
+      console.log('got a response');
+    });
+  }
+
+  render() {
+    return (
+      <div className='hello-world'>
+        <h1>Images will be displayed here</h1>
+      </div>
+    )
+  }
 }
-export default ImageViewer
+export default ImageViewer 
