05446facb1c56445a8f7b03eb931216cde2f1a0d
Gauri Bodhankar <gbodhankar@stubhub.com>
2018-02-08 16:59:23 -0800

    Formatting changes

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Image from './Image.js'
 import Error from './Error.js'
 import dateFormat from 'dateformat'
@@ -25,8 +25,8 @@ class ImageViewer extends Component {
     this.imageFetchCount = 30;  // number of images to fetch on each call
     this.startIndex = 0;
     this.endIndex = this.startIndex + this.imageFetchCount;
-  
-    this.isLastImageFetched = false;
+
+    this.isLastImageFetched = false;  // to check if all images have been fetched
 
     this.handleImageError = this.handleImageError.bind(this);
     this.handleOnScroll = this.handleOnScroll.bind(this);
@@ -42,61 +42,61 @@ class ImageViewer extends Component {
 
   componentWillUpdate(nextProps, nextState) {
     // checking for valid drag-drop operation to reorder
-    if((nextState.dragIndex !== null && nextState.dropIndex !== null) && nextState.dragIndex !== nextState.dropIndex) {
+    if ((nextState.dragIndex !== null && nextState.dropIndex !== null) && nextState.dragIndex !== nextState.dropIndex) {
       const reorderedImages = Object.assign([], this.state.loadedImages);
       reorderedImages[nextState.dragIndex] = this.state.loadedImages[nextState.dropIndex];
       reorderedImages[nextState.dropIndex] = this.state.loadedImages[nextState.dragIndex];
-      this.setState({loadedImages: reorderedImages, dragIndex: null, dropIndex: null});
+      this.setState({ loadedImages: reorderedImages, dragIndex: null, dropIndex: null });
     }
   }
-  
+
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleOnScroll, false);
   }
 
   fetchImages() {
-    this.setState({loading: true});
+    this.setState({ loading: true });
     fetchImages('/data/imageData.json', this.startIndex, this.endIndex).then((imageData) => {
       try {
-        if(imageData.images) {
+        if (imageData.images) {
           const imageArray = [];
           imageData.images.reduce((prevImage, image) => {
             prevImage = prevImage || {};
-    
+
             // combining the Converted Image and Deployment Target objects (assuming that both appear consecutively in the response)
-            if(prevImage.assetId === image.assetId) {
+            if (prevImage.assetId === image.assetId) {
               Object.assign(prevImage, image);
-              
+
               prevImage.url = `https://secure.netflix.com/us/boxshots/${prevImage.dir}/${prevImage.filename}`;
               prevImage.deploymentTs = dateFormat(new Date(image.deploymentTs), "mm/dd/yyyy hh:mm");
-              
+
               imageArray.push(prevImage);
             } else {
               prevImage = image;
             }
-              return prevImage;
-            });
-            this.isLastImageFetched = imageData.isLastImageFetched;
+            return prevImage;
+          });
+          this.isLastImageFetched = imageData.isLastImageFetched;
 
-            this.setState({
-              loadedImages: imageArray,
-              loading: false,
-              errorClass: 'hidden',
-              errorMsg: ''
-            });
+          this.setState({
+            loadedImages: imageArray,
+            loading: false,
+            errorClass: 'hidden',
+            errorMsg: ''
+          });
         }
         // throw new Error('test'); // TODO: getting an error here
-      } catch(error) {
+      } catch (error) {
         this.setErrorState(error);
       }
     }).catch((error) => {
-     this.setErrorState(error);
+      this.setErrorState(error);
     });
   }
 
   setErrorState(error) {
     const defaultErrorMsg = 'Something went wrong. Please try again.'
-    this.setState({ 
+    this.setState({
       loading: false,
       errorClass: '',
       errorMsg: error || defaultErrorMsg
@@ -108,22 +108,22 @@ class ImageViewer extends Component {
   }
 
   handleOnScroll() {
-    if(!this.state.loading) {
+    if (!this.state.loading) {
       this.prevScrollTop = this.currentScrollTop;
-      this.currentScrollTop =  window.scrollY;
-      if(this.currentScrollTop < this.prevScrollTop) {  // handling scroll up
-        if((window.scrollY < 700 || window.scrollY === 0)  && (this.startIndex !== 0 && (this.state.loadedImages.length === this.threshold || this.isLastImageFetched))) {
-          console.log('iffff', this.startIndex, this.endIndex, this.state.loadedImages.length);
-          this.startIndex = (this.startIndex - this.imageFetchCount) < 0 ? 0 : (this.startIndex - this.imageFetchCount); 
-          this.endIndex = (this.endIndex - this.imageFetchCount) < 0 ? 0 : (this.endIndex - this.imageFetchCount); 
+      this.currentScrollTop = window.scrollY;
+      // console.log('loadedImages', this.state.loadedImages.length);
+      if (this.currentScrollTop < this.prevScrollTop) {  // handling scroll up
+        if ((window.scrollY < 1000 || window.scrollY === 0) && (this.startIndex !== 0 && (this.state.loadedImages.length === this.threshold || this.isLastImageFetched))) {
+          this.startIndex = (this.startIndex - this.imageFetchCount) < 0 ? 0 : (this.startIndex - this.imageFetchCount);
+          this.endIndex = (this.endIndex - this.imageFetchCount);
           this.fetchImages();
           // console.log('up', this.startIndex, this.endIndex, this.state.loadedImages.length);
         }
-      } else if(this.currentScrollTop > this.prevScrollTop) { // handling scroll down
-        if((window.innerHeight + window.scrollY) >= (document.getElementById('images-container').clientHeight - 500)) {
-          console.log(this.isLastImageFetched);
-          if(!this.isLastImageFetched) {
-            if(!(this.state.loadedImages.length < this.threshold)) {
+      } else if (this.currentScrollTop > this.prevScrollTop) { // handling scroll down
+        if ((window.innerHeight + window.scrollY) >= (document.getElementById('images-container').clientHeight - 1000)) {
+          console.log('y ',window.scrollY);
+          if (!this.isLastImageFetched) {
+            if (!(this.state.loadedImages.length < this.threshold)) {
               this.startIndex = this.startIndex + this.imageFetchCount;
             }
             this.endIndex = this.endIndex + this.imageFetchCount;
@@ -139,7 +139,7 @@ class ImageViewer extends Component {
     return (
       <div className='page'>
         <div id='error-container' className={`error-container ${this.state.errorClass}`}>
-          <Error 
+          <Error
             errorMessage={this.state.errorMsg}>
           </Error>
         </div>
@@ -153,24 +153,24 @@ class ImageViewer extends Component {
               width={image.width}
               movieId={image.movieId}
               deploymentTs={image.deploymentTs}
-              handleDragStart={(dragIndex)=> {
-                this.setState({dragIndex});
+              handleDragStart={(dragIndex) => {
+                this.setState({ dragIndex });
               }}
               handleDragOver={(event) => {
                 event.preventDefault();
               }}
-              handleDrop={(dropIndex)=> {
-                this.setState({dropIndex});
+              handleDrop={(dropIndex) => {
+                this.setState({ dropIndex });
               }}
               handleImageError={this.handleImageError}>
             </Image>;
           })}
         </div>
         <div className='loading'>
-            <BeatLoader
-              color={'gray'} 
-              loading={this.state.loading} 
-            />
+          <BeatLoader
+            color={'gray'}
+            loading={this.state.loading}
+          />
         </div>
       </div>
     )
--- src/helpers/fetchHandler.js
+++ src/helpers/fetchHandler.js
@@ -1,26 +1,25 @@
+export const fetchImages = (url, startIndex, endIndex) => new Promise((resolve, reject) => {
+    fetch(
+        url
+    ).then(response => {
+        return response.json();
+    }).then((imageData) => {
+        const totalImages = imageData.length;
+        if (imageData && totalImages > 0) {
+            console.log('fetchHandler before > ', endIndex);
+            const isLastImageFetched = totalImages < endIndex * 2;
+            endIndex = isLastImageFetched ? totalImages : endIndex * 2; // adjusting the end index if at end of image array
 
-   export const fetchImages = (url, startIndex, endIndex) => new Promise((resolve, reject) => {
-        fetch(
-            url
-          ).then(response => {
-            return response.json();
-          }).then((imageData) => {
-            const totalImages = imageData.length;
-            if(imageData && totalImages > 0) {
-                const isLastImageFetched = totalImages < endIndex*2;
-                endIndex = isLastImageFetched ? totalImages : endIndex*2;
-
-                // console.log('fetchHandler > ', totalImages, startIndex, endIndex);
-                // console.log('isLastImageFetched > ', isLastImageFetched);
-                resolve({
-                    images: imageData.slice(startIndex*2, endIndex),
-                    isLastImageFetched
-                });
-            } else {
-                reject('No images available. Please try again.');
-            }         
-          }).catch((error) => {
-            reject(error && error.toString()); // TODO: do we show technical errors to users?
-          });
+            // console.log('fetchHandler > ', totalImages, startIndex, endIndex);
+            // console.log('isLastImageFetched > ', isLastImageFetched);
+            resolve({
+                images: imageData.slice(startIndex * 2, endIndex),
+                isLastImageFetched
+            });
+        } else {
+            reject('No images available. Please try again.');
+        }
+    }).catch((error) => {
+        reject(error && error.toString()); // TODO: do we show technical errors to users?
     });
-
+});
