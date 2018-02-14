23b34e5f110be8ffd01eebacf7780f328d24a862
Gauri Bodhankar <gbodhankar@stubhub.com>
2018-02-13 16:26:40 -0800

    Moved files outside src, hence deleting these

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ImageViewer from './components/ImageViewer.jsx'
-
-ReactDOM.render(<ImageViewer 
-    threshold={180}          // maximum number of images that can be loaded on the page at a given time
-    imageFetchCount={90}    // number of images to fetch on each call
-/>, document.getElementById('app'))
