7d8c517658caffb0c5b92ad53b06fd65def44a6e
Gauri Bodhankar <gbodhankar@stubhub.com>
2018-04-29 20:53:37 -0700

    Updated UI

--- src/components/CalculatorDisplay.jsx
+++ src/components/CalculatorDisplay.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const CalulatorDisplay = (props) => {
 
     return (
-        <div>
-            <label className="input-output-display">
-                {props.input}
-            </label>
-        </div>
+        <label className="input-output-display">
+            {props.input}
+        </label>
     );
 }
 
